2a019497bbb49fc722a718fc5cc0d027e9e616e8
codesusuzz <hluvee@naver.com>
2023-03-28 23:13:14 +0900

    [Feat] Mail content prediction API 생성, Smart Scan API 완성

--- app.py
+++ app.py
@@ -10,54 +10,82 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
+import pymysql
+
 app = Flask(__name__)
 
-@app.route('/predict/', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        df=pd.read_csv('./maildataset.csv',encoding='latin-1')
+@app.route('/smart-scan', methods=['GET'])
+def smart_scan():
+    username = request.args.get('username')
+    content = db_connector(username)
+
+    removable_idx = []
+    result = predict(content)
+    return jsonify(result)
+
+
+def predict(contents):
+    df=pd.read_csv('./maildataset.csv',encoding='latin-1')
         
-        df1=df.where(pd.notnull(df),'')
-        df1["v1"].replace('ham',1,inplace=True)
-        df1["v1"].replace('spam',0,inplace=True)
+    df1=df.where(pd.notnull(df),'')
+    df1["v1"].replace('ham',1,inplace=True)
+    df1["v1"].replace('spam',0,inplace=True)
 
-        x=df1["v2"]
-        y=df1["v1"]
+    x=df1["v2"]
+    y=df1["v1"]
 
-        x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.8,random_state=0)
+    x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.8,random_state=0)
 
-        feature_extraction= TfidfVectorizer(min_df=1,stop_words='english',lowercase= True)
-        x_train_feature=feature_extraction.fit_transform(x_train)
-        x_test_feature=feature_extraction.transform(x_test)
+    feature_extraction= TfidfVectorizer(min_df=1,stop_words='english',lowercase= True)
+    x_train_feature=feature_extraction.fit_transform(x_train)
+    x_test_feature=feature_extraction.transform(x_test)
 
-        y_train=y_train.astype('int')
-        y_test=y_test.astype('int')
+    y_train=y_train.astype('int')
+    y_test=y_test.astype('int')
 
-        model_from_joblib = joblib.load('./mail_model.pkl')
-        model_from_joblib.fit(x_train_feature,y_train)
+    model_from_joblib = joblib.load('./mail_model.pkl')
+    model_from_joblib.fit(x_train_feature,y_train)
 
-        # 메일 내용 -> 모델 예측 요청
-        parser=reqparse.RequestParser()
-        parser.add_argument('msg',type=str)
-
-        input_mail=[]
-        args=parser.parse_args()
-        input_mail.append(args['msg'])
-        print(input_mail)
+    # 메일 내용 -> 모델 예측 요청
+    removable_idx = []
+    for content in contents:
+        input_mail= [content[2]]
 
         # 벡터로 변환
         input_data_features = feature_extraction.transform(input_mail)
 
         # 예측
         prediction = model_from_joblib.predict(input_data_features)
-        print(prediction)
 
         if (prediction[0]==1):
-            result='Ham mail'
+            print("정상 메일로 분류되어 삭제하지 않습니다")
         else:
-            result='Spam mail'
+            # 삭제할 mail_id 를 추가
+            removable_idx.append(content[0])
+            print("\n\n\n스팸 메일로 분류되었습니다")
 
-    return jsonify(result), 200
+    return removable_idx
+
+
+def db_connector(username):
+    db = {
+
+        'user': 'root',
+        'password': 'qwerty1234',
+        'host': '34.22.71.172',
+        'port': 3306,
+        'database': 'marbon'
+    }
+    db_connection = pymysql.connect(host='34.22.71.172',
+                                    port=3306,
+                                    user='root',
+                                    password='qwerty1234',
+                                    charset='utf8mb4')
+    cursor = db_connection.cursor()
+    sql = "SELECT * FROM marbon.mail_data where username=%s"
+
+    cursor.execute(sql, username)
+    return cursor.fetchall()
 
 if __name__=="__app__":
     app.run(debug=True)
